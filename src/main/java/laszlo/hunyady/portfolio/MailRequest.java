9b8bce1760d09289396194dd9e62597ea3e9acaa
Laci <hu.laszlo93@gmail.com>
2020-11-26 18:05:00 +0100

    Added body for email
    Added the body for the email request, and extended sanity test.

--- src/main/java/laszlo/hunyady/portfolio/MailRequest.java
+++ src/main/java/laszlo/hunyady/portfolio/MailRequest.java
@@ -1,13 +1,23 @@
 package laszlo.hunyady.portfolio;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MailRequest {
+    @NotEmpty
     private String name;
+    @Email
     private String email;
+    @NotEmpty
     private String message;
 
 }
--- src/test/java/laszlo/hunyady/portfolio/rule/MailRequestHelper.java
+++ src/test/java/laszlo/hunyady/portfolio/rule/MailRequestHelper.java
@@ -3,10 +3,19 @@ package laszlo.hunyady.portfolio.rule;
 import laszlo.hunyady.portfolio.MailRequest;
 
 public class MailRequestHelper {
+
+    public static final String TEST_NAME = "TestName";
+    public static final String TEST_EMAIL = "test@test.test";
+    public static final String TEST_MESSAGE = "testMessage";
+
     private MailRequestHelper() {
     }
 
     public static MailRequest getValidEmail() {
-        return MailRequest.builder().name("TestName").email("test@test.test").message("testMessage").build();
+        return MailRequest.builder().name(TEST_NAME).email(TEST_EMAIL).message(TEST_MESSAGE).build();
+    }
+
+    public static String buildTestSubject() {
+        return TEST_EMAIL + " - " + TEST_NAME;
     }
 }
