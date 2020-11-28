077c567d94670d63913989a2616c91bba03901ad
Laci <hu.laszlo93@gmail.com>
2020-11-28 21:36:25 +0100

    Fine tuned htmls
    - added fragments to support modularity
    - added localization support
    - did some refactioring (MailContextHelper)
    - added a thank you mail

---
+++ src/main/java/laszlo/hunyady/portfolio/mail/MailContextHelper.java
@@ -0,0 +1,41 @@
+package laszlo.hunyady.portfolio.mail;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+
+@Slf4j
+@Component
+public class MailContextHelper {
+
+    @Value("${mail.thanks.name.pre}")
+    public String NAME_PRE;
+    @Value("${mail.thanks.name.post}")
+    public String NAME_POST;
+    @Value("${mail.thanks.body.email.pre}")
+    public String BODY_EMAIL_PRE;
+    @Value("${mail.thanks.body.email.post}")
+    public String BODY_EMAIL_POST;
+    @Value("${mail.thanks.farewell}")
+    public String FAREWELL;
+    @Value("${mail.thanks.sign}")
+    public String SIGN;
+
+    public Context buildContactMailContext(MailRequest mail) {
+        Context context = new Context();
+        context.setVariable("message", mail.getMessage());
+        context.setVariable("name", mail.getName());
+        context.setVariable("email", mail.getEmail());
+        return context;
+    }
+
+    public Context buildThanksMailContext(MailRequest mail) {
+        Context context = new Context();
+        context.setVariable("greetings", NAME_PRE + mail.getName() + NAME_POST);
+        context.setVariable("mail_body", BODY_EMAIL_PRE + mail.getEmail() + BODY_EMAIL_POST);
+        context.setVariable("farewell", FAREWELL);
+        context.setVariable("sign", SIGN);
+        return context;
+    }
+}
--- src/main/java/laszlo/hunyady/portfolio/mail/MailServiceImpl.java
+++ src/main/java/laszlo/hunyady/portfolio/mail/MailServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerErrorException;
-import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
@@ -15,38 +14,47 @@ import java.nio.charset.StandardCharsets;
 
 @Service
 public class MailServiceImpl implements MailService {
-    @Value("${mail.to.address}")
-    private String mailTarget;
+    public static final String CONTACT_THANK_YOU_MAIL = "contact_thank_you_mail";
+    public static final String MAIL = "mail";
+
+    @Value("${mail.contact.subject}")
+    private String contact;
+    @Value("${mail.bot.address}")
+    private String mailBotAddress;
 
     @Autowired
+    private MailContextHelper contextHelper;
+    @Autowired
     private JavaMailSender mailSender;
-
     @Autowired
     private SpringTemplateEngine templateEngine;
 
     public void send(MailRequest mail) {
         try {
-            mailSender.send(buildMailForm(mail));
+            mailSender.send(buildContactMail(mail));
+            mailSender.send(buildThanksMail(mail));
         } catch (MessagingException e) {
             throw new ServerErrorException("Not proper mail data.", e);
         }
     }
 
-    private MimeMessage buildMailForm(MailRequest mail) throws MessagingException {
+    private MimeMessage buildContactMail(MailRequest mail) throws MessagingException {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
-
-        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
-        message.setFrom(mail.getEmail());
-        message.setTo(mailTarget);
-        message.setSubject(mail.getEmail() + " - " + mail.getName());
-        message.setText(templateEngine.process("mail", buildThymeleafContext(mail)), true);
-
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+        helper.setFrom(mail.getEmail());
+        helper.setTo(mailBotAddress);
+        helper.setSubject(mail.getEmail() + " - " + mail.getName());
+        helper.setText(templateEngine.process(MAIL, contextHelper.buildContactMailContext(mail)), true);
         return mimeMessage;
     }
 
-    private Context buildThymeleafContext(MailRequest mail) {
-        Context context = new Context();
-        context.setVariable("message", mail.getMessage());
-        return context;
+    private MimeMessage buildThanksMail(MailRequest mail) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+        helper.setFrom(mailBotAddress);
+        helper.setTo(mail.getEmail());
+        helper.setSubject(contact);
+        helper.setText(templateEngine.process(CONTACT_THANK_YOU_MAIL, contextHelper.buildThanksMailContext(mail)), true);
+        return mimeMessage;
     }
 }
